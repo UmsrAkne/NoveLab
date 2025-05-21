9dc90d0b4b3b0d4c63ab462f3f26ba80b08fce1e
UmsrAkne <sibata486@gmail.com>
2025-05-21 12:46:14 +0900

    feat: add BGM playback component to ScenarioScene and play test BGM

--- Assets/Scripts/Scenes/Scenario/ScenarioManager.cs
+++ Assets/Scripts/Scenes/Scenario/ScenarioManager.cs
@@ -1,9 +1,33 @@
+using Audio;
+using Cysharp.Threading.Tasks;
+using Loaders;
 using UnityEngine;
 
 namespace Scenes.Scenario
 {
     public class ScenarioManager : MonoBehaviour
     {
+        [SerializeField]
+        private AudioLoader audioLoader;
 
+        [SerializeField]
+        private AudioSource audioSource;
+
+        [SerializeField]
+        private BgmPlayer bgmPlayer;
+
+        private void Start()
+        {
+            var path = @"C:\Users\Public\testData\sounds\list2\music.ogg";
+            Debug.Log(path);
+            LoadSound(path).Forget();
+        }
+
+        private async UniTaskVoid LoadSound(string path)
+        {
+            await audioLoader.LoadAudioClipAsync(path);
+            var clip = audioLoader.GetCachedClip(path);
+            await bgmPlayer.PlayBgmAsync(clip, 5f);
+        }
     }
 }
