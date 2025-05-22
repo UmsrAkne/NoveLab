2694f5d09bbbf2b4bcd621ab528bc1312f76505f
UmsrAkne <sibata486@gmail.com>
2025-05-23 01:35:34 +0900

    feat: add ImageSet class to manage multi-layered character images

---
+++ Assets/Scripts/UI/Images/ImageSet.cs
@@ -0,0 +1,99 @@
+using System.Collections.Generic;
+using Core;
+using UI.Adapters;
+using UnityEngine;
+
+namespace UI.Images
+{
+    public class ImageSet : MonoBehaviour, IDisplayImage
+    {
+        [SerializeField]
+        private CanvasGroup canvasGroup;
+        private readonly Dictionary<string, IUIAnimation> animations = new();
+
+        [SerializeField]
+        private SpriteAdapter baseImage;
+
+        [SerializeField]
+        private SpriteAdapter eye;
+
+        [SerializeField]
+        private SpriteAdapter mouth;
+
+        public GameObject GameObject => gameObject;
+
+        public void SetExpression(Texture2D eyeTex, Texture2D mouthTex)
+        {
+            eye.SetTexture(eyeTex);
+            mouth.SetTexture(mouthTex);
+        }
+
+        public void SetTextures(Texture2D baseTexture, Texture2D eyeTex, Texture2D mouthTex)
+        {
+            baseImage.SetTexture(baseTexture);
+            eye.SetTexture(eyeTex);
+            mouth.SetTexture(mouthTex);
+        }
+
+        public void SetAlpha(float alpha)
+        {
+            if (canvasGroup != null)
+            {
+                canvasGroup.alpha = alpha;
+            }
+        }
+
+        public void SetScale(float scale)
+        {
+            var rect = GameObject.transform as RectTransform;
+            if (rect != null)
+            {
+                rect.localScale = new Vector3(scale, scale, 1f);
+            }
+        }
+
+        public void SetTexture(Texture2D baseTexture)
+        {
+            baseImage.SetTexture(baseTexture);
+        }
+
+        public void RegisterAnimation(string key, IUIAnimation anime)
+        {
+            if (animations.ContainsKey(key))
+            {
+                Debug.LogWarning($"Animation with key '{key}' already registered. Overwriting.");
+            }
+
+            animations[key] = anime;
+
+            anime.OnCompleted += () =>
+            {
+                if (!animations.ContainsKey(key))
+                {
+                    return;
+                }
+
+                animations.Remove(key);
+                Debug.Log($"Animation '{key}' completed and removed from registry.");
+            };
+        }
+
+        public void PlayAnimations()
+        {
+            foreach (var keyValuePair in animations)
+            {
+                keyValuePair.Value.Start();
+            }
+        }
+
+        public void SetBasePosition(Vector2 pos)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetOffsetPosition(Vector2 offset)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
