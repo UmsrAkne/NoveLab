71dc0c8802e9b835db4cf4a2a1df9d0172f9b051
UmsrAkne <sibata486@gmail.com>
2025-07-16 15:50:34 +0900

    feat: introduce XML scenario parser interface and base implementation
    - Added IXElementParser interface for parsing <scenario> elements into ScenarioEntry
    - Created initial implementation TextElementParser with PopulateScenario stub
    - Required Core reference to access ScenarioEntry in XML parser

---
+++ Assets/Scripts/Loaders/XMLs/IXElementParser.cs
@@ -0,0 +1,15 @@
+using System.Xml.Linq;
+using Scenes.Scenario;
+
+namespace Loaders.XMLs
+{
+    public interface IXElementParser
+    {
+        /// <summary>
+        /// 入力した Scenario 要素をパースし、ScenarioEntry に入力します。
+        /// </summary>
+        /// <param name="scenarioElement"></param>
+        /// <param name="scenario"></param>
+        void PopulateScenario(XElement scenarioElement, ScenarioEntry scenario);
+    }
+}
---
+++ Assets/Scripts/Loaders/XMLs/TextElementParser.cs
@@ -0,0 +1,12 @@
+using System.Xml.Linq;
+using Scenes.Scenario;
+
+namespace Loaders.XMLs
+{
+    public class TextElementParser : IXElementParser
+    {
+        public void PopulateScenario(XElement scenarioElement, ScenarioEntry scenario)
+        {
+        }
+    }
+}
