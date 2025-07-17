3b918eee4984bc32232deb7b546bd1a01f555e62
UmsrAkne <sibata486@gmail.com>
2025-07-17 22:05:45 +0900

    refactor: move XmlHelper to Loader.XMLs and start using it in TextElementParser
    - Relocated XmlHelper from UI.Utils to Loader.XMLs due to inappropriate placement
    - XmlHelper is now used by TextElementParser for reading <text> elements

--- Assets/Scripts/Loaders/XMLs/TextElementParser.cs
+++ Assets/Scripts/Loaders/XMLs/TextElementParser.cs
@@ -1,5 +1,6 @@
 using System.Xml.Linq;
 using Scenes.Scenario;
+using UI.Utils;
 
 namespace Loaders.XMLs
 {
@@ -7,6 +8,16 @@ namespace Loaders.XMLs
     {
         public void PopulateScenario(XElement scenarioElement, ScenarioEntry scenario)
         {
+            if (!XmlHelper.HasChild(scenarioElement, "text"))
+            {
+                return;
+            }
+
+            var text = XmlHelper.GetStringAttribute(scenarioElement, "string");
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                scenario.Text = text;
+            }
         }
     }
 }
